d3304d34164ad0e6f122a7cbcf526a96cb39422e
Joost van der Laan <joostvanderlaan@gmail.com>
2016-05-21 00:36:07 +0200

    Serve with BrowserSync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -258,3 +258,26 @@ return exec(cmd)
  });
 
 });
+
+// Watch files for changes & reload
+gulp.task('serve', ['scripts', 'styles'], () => {
+  browserSync({
+    notify: false,
+    // Customize the Browsersync console logging prefix
+    logPrefix: 'WSK',
+    // Allow scroll syncing across breakpoints
+    scrollElementMapping: ['main', '.mdl-layout'],
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: ['.tmp', 'app', '../../public'], // public is where Hugo outputs its stuff
+    files: ['../../public/**'],
+    port: 3000
+  });
+
+  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
+  gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts']);
+  gulp.watch(['app/images/**/*'], reload);
+});
